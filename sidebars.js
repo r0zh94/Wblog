80150a184d2a4daa8edc408e98eca6555c083e01
r0zh94 <r0zh@outlook.com>
2022-02-20 22:34:08 +0800

    master

--- sidebars.js
+++ sidebars.js
@@ -2,10 +2,13 @@ module.exports = {
   docs: [
     'Home',
 
-   // {
-    //  type: 'category',
-    //  label: '硬件设计 - 应用方案',
-    //  items: [
-    //    'OSD335x最小系统的设计',
+   {
+    type: 'category',
+      label: '天猫&淘宝',
+      items: [
+        'OSD335x最小系统的设计',
      ],
     },
+    
+  ],
+};
